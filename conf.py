1c48cd3ac54ea6923e95a88491b033c037eb8986
Mikhail Riabtsev <rabtsevby@tut.by>
2026-01-21 07:11:20 -0600

    fff

--- conf.py
+++ conf.py
@@ -6,7 +6,5 @@ DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 DATA_DIR = os.getenv("DATA_DIR", "data/")
 OUTPUT_DIR = os.getenv("OUTPUT_DIR", "output/")
 
-SVG_URL = os.getenv(
-    "SVG_URL",
-    "https://raw.githubusercontent.com/Mihailby/articles/main/pls.svg"
-)
+SVG_URL = "https://raw.githubusercontent.com/Mihailby/articles/main/pls.svg"
+THEME = "auto"   # "dark", "light", "auto"
